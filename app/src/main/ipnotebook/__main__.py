492a98f7a696a15351485e655db34f6db421d3a1
NativCos <None@None>
2020-11-11 16:58:16 +0300

    create

---
+++ app/src/main/ipnotebook/__main__.py
@@ -0,0 +1,23 @@
+"""
+Run program
+"""
+
+import sys
+from PyQt5 import QtWidgets
+from .mainwindow import MainWindow
+import logging
+
+
+FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=FORMAT)
+
+
+def run():
+    app = QtWidgets.QApplication(sys.argv)
+    app.setApplicationDisplayName("IP Notebook")
+    app.setApplicationName("IP Notebook")
+    mainWindow = MainWindow()
+    app.exec_()
+
+
+run()
