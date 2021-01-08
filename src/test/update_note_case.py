0ff2149de986ccd7cc50552b450167bd7f6e3900
NativCos <None@None>
2021-01-08 23:42:12 +0300

    fix test update_note_case.py - v2

--- src/test/update_note_case.py
+++ src/test/update_note_case.py
@@ -9,14 +9,14 @@ from ipnotebook.program import DataDontValidException
 logging.root.setLevel(logging.INFO)
 
 
-class NumberOfMarksCase(unittest.TestCase):
-    """ Тест количества меток """
+class MarksCase(unittest.TestCase):
+    """ Тест меток """
     def setUp(self) -> None:
         with open(os.path.join('resources', 'test.ntip'), 'r') as f:
             json_dict = json.load(fp=f)
         self._notebook = NoteBook.from_json(json_dict)
 
-    def test_marks_0(self):
+    def test_number_0(self):
         note_id = 1
         try:
             self._notebook.update_note(note_id, '', '')
@@ -25,18 +25,7 @@ class NumberOfMarksCase(unittest.TestCase):
         except DataDontValidException:
             self.assertTrue(False)
 
-    def test_marks_1(self):
-        marks_set = {'a'}
-        marks_str = ' '.join(marks_set)
-        note_id = 1
-        try:
-            self._notebook.update_note(note_id, marks_str, '')
-            self.assertEqual(self._notebook.get_note(note_id).marks, marks_set)
-            self.assertTrue(True)
-        except DataDontValidException:
-            self.assertTrue(False)
-
-    def test_marks_10(self):
+    def test_number_10_len_1(self):
         marks_set = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'}
         marks_str = ' '.join(marks_set)
         note_id = 1
@@ -47,8 +36,19 @@ class NumberOfMarksCase(unittest.TestCase):
         except DataDontValidException:
             self.assertTrue(False)
 
-    def test_marks_11(self):
-        marks_set = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a'}
+    def test_number_10_len_16(self):
+        marks_set = {'q'*16, 'w'*16, 'e'*16, 'r'*16, 't'*16, 'y'*16, 'u'*16, 'i'*16, 'o'*16, 'p'*16}
+        marks_str = ' '.join(marks_set)
+        note_id = 1
+        try:
+            self._notebook.update_note(note_id, marks_str, '')
+            self.assertEqual(self._notebook.get_note(note_id).marks, marks_set)
+            self.assertTrue(True)
+        except DataDontValidException:
+            self.assertTrue(False)
+
+    def test_number_10_len_17(self):
+        marks_set = {'q'*17, 'w'*17, 'e'*17, 'r'*17, 't'*17, 'y'*17, 'u'*17, 'i'*17, 'o'*17, 'p'*17}
         marks_str = ' '.join(marks_set)
         note_id = 1
         try:
@@ -57,45 +57,15 @@ class NumberOfMarksCase(unittest.TestCase):
         except DataDontValidException:
             self.assertTrue(True, "тест пройден. вернули исключение, как и положено")
 
-
-class LengthOfMarksCase(unittest.TestCase):
-    """ Тест длины метки """
-    def setUp(self) -> None:
-        with open(os.path.join('resources', 'test.ntip'), 'r') as f:
-            json_dict = json.load(fp=f)
-        self._notebook = NoteBook.from_json(json_dict)
-
-    def test_length_1(self):
-        marks_set = {'a'}
-        marks_str = ' '.join(marks_set)
-        note_id = 1
-        try:
-            self._notebook.update_note(note_id, marks_str, '')
-            self.assertEqual(len(self._notebook.get_note(note_id).marks.pop()), 1)
-            self.assertTrue(True)
-        except DataDontValidException:
-            self.assertTrue(False)
-
-    def test_length_16(self):
-        marks_set = {'a'*16}
-        marks_str = ' '.join(marks_set)
-        note_id = 1
-        try:
-            self._notebook.update_note(note_id, marks_str, '')
-            self.assertEqual(len(self._notebook.get_note(note_id).marks.pop()), 16)
-            self.assertTrue(True)
-        except DataDontValidException:
-            self.assertTrue(False)
-
-    def test_length_17(self):
-        marks_set = {'a'*17}
+    def test_number_11_len_1(self):
+        marks_set = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'm'}
         marks_str = ' '.join(marks_set)
         note_id = 1
         try:
             self._notebook.update_note(note_id, marks_str, '')
             self.assertTrue(False, "тест не пройден. не было исключения о некоррестных введенных данных")
         except DataDontValidException:
-            self.assertTrue(True, "тест пройден. вернули исключение, как и положено")
+            self.assertTrue(True)
 
 
 class LengthTextCase(unittest.TestCase):
